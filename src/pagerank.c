89a43a1d8805e6423ab80bcb5ae7022c179d2069
Frédéric Hannes <freddy1990@gmail.com>
2014-03-22 00:57:23 +0100

    New implementation (WIP)

--- src/pagerank.c
+++ src/pagerank.c
@@ -5,108 +5,203 @@
 #include <string.h>
 #include <time.h>
 
+// Size of the link matrix
 static size_t matrix_size;
-static int count;
-static const double fudge_factor = 0.9;
-static double vector[4];
-static double vector_tmp[4];
 
-//initialisation function for ip
-void ip_init( double **ip_buffer ) {
-	const size_t size = bsp_nprocs() * sizeof(double);
-	*ip_buffer = malloc( size );
-	bsp_push_reg( *ip_buffer, size );
-}
+// 1 / matrix_size, used to avoid recalculating this static value
+static double size_div;
 
-//calculates the inner-product from local vectors
-double ip( double *x, double *y, double *ip_buffer, size_t np ) {
-	double alpha = 0.0;
+// A vector containing the number of non-zero elements for each row
+static size_t *nonzero_vector;
 
-	for( size_t i = 0; i < np; ++i ){
-		//printf("In thread %d: value of x[%d] = %f and y=[%d] = %f \n",bsp_pid(),i,x[i],i,y[i]);
-		alpha += fudge_factor * x[ i ] * y[ i ];
-	}
-	for( unsigned int k = 0; k < bsp_nprocs(); ++k ) {
-		bsp_put( k, &alpha, ip_buffer,bsp_pid()*sizeof(double), sizeof(double) );
-	}
-	bsp_sync();
-	for( unsigned int k = 1; k < bsp_nprocs(); ++k )
-		ip_buffer[ 0 ] += ip_buffer[ k ];
-	return ip_buffer[ 0 ];
-}
+/**
+ * Link matrix which will be transformed into the Google matrix during execution.
+ * NOTE: The matrix is stored by column rather than row, to allow for easier inner-
+ *       product calculation.
+ */
+static double *google_matrix;
 
-//Vb: {{1,2,3}{4,5,6}{7,8,9}} --> {1,4,7,2,5,8,3,6,9}
-//deze functie vult een array met een geflattende 2D array volgens rij
-void fill_matrix(size_t M, double matrix[M*M]) {
-	unsigned int x,y;
-	for(x=0;x<M;x++){
-		for(y=0;y<M;y++){
-			matrix[x*M + y] = y*M + x;
+// Array offsets for matrix_size over nprocs processors
+static size_t *offsets;
+
+/**
+ * A constant factor (between 0 and 1) which indicates how often users will stay
+ * within the hyperlink network, rather than jump to a random page.
+ */
+static const double ALPHA = 0.9;
+
+/**
+ * The actual PageRank vector.
+ */
+static double *pagerank_vector;
+
+/**
+ * The number of iterations of the power method.
+ */
+static const double ITERATIONS = 50;
+
+void gm_spmd() {
+	size_t nprocs = bsp_nprocs();
+
+	// Start BSP program (Simple threaded program, no race conditions)
+	bsp_begin(nprocs);
+
+	/**
+	 * 1) Normalize the link matrix to transform into the hyperlink matrix:
+	 * 		The original matrix contains a 1 for each link from one page to another,
+	 * 		this step of the process will transform the matrix into a representation
+	 * 		of the markov chain model associated with the original link matrix. This
+	 * 		newly formed matrix is referred to as the hyperlink matrix.
+	 * 2) Stochasticity adjustment:
+	 * 		The hyperlink matrix can contain rows with only zeros when the associated
+	 * 		page does not link to any other page or at least not any page that is
+	 * 		present in the matrix. When running the PageRank algorithm, these rows
+	 * 		create "rank sinks". As the rank of a page is determined by the rank of
+	 * 		the incoming pages, these end-nodes in the graph will accumulate more and
+	 * 		more of the rank and thereby monopolize it, as they never share it to any
+	 * 		of the other pages. The stochasticity adjustment counters this issue by
+	 * 		emulating an outlink from these pages to all other pages, causing their
+	 * 		rank to be distributed across all other pages each iteration.
+	 * 3) Primitivity adjustment:
+	 *		The stochastic matrix can't assure that a positive PageRank value exists
+	 *		and that calculation with the power method will converge fast enough. A
+	 *		value alpha was derived from the "random surfer" argument which takes
+	 *		into account that a surfer may jump to a random page outside of the
+	 *		current hyperlink structure. This adjustment also converts the matrix
+	 *		into a dense matrix. This implementation of the PageRank algorithm uses
+	 *		this dense matrix for calculations, but due to the nature of all of the
+	 *		previous operations, there really is no need to store the entire matrix,
+	 *		as the updated zero values can easily be determined during runtime.
+	 */
+	for (int x = offsets[bsp_pid()]; x < offsets[bsp_pid() + 1]; x++) {
+		int pos = x * matrix_size;
+		for (int y = 0; y < matrix_size; y++) {
+			double value = 0;
+			int elem_count = nonzero_vector[y];
+			if (elem_count != 0) {
+				value = google_matrix[pos] / elem_count;
+			} else {
+				value = size_div;
+			}
+			value = ALPHA * value + (1 - ALPHA) * size_div;
+			google_matrix[pos++] = value;
 		}
 	}
+
+	// End the BSP program
+	bsp_end();
 }
 
-void spmd() {
-	double matrix[] = {0.2,0.2,0.5,0.0,0.2,0.2,0.0,0.0,0.2,0.2,0.0,0.0,0.2,0.2,0.0,1}; //test matrix
-	//double matrix[M*M];
-	//fill_matrix(M, matrix);
+void pr_spmd() {
+	size_t nprocs = bsp_nprocs();
 
-	size_t procs = bsp_nprocs();
-	if (procs > matrix_size)
-		procs = matrix_size;
+	// Start BSP program
+	bsp_begin(nprocs);
 
-	bsp_begin(procs);
-	double *ip_buffer, *x, *y;
-	size_t np;
-	ip_init( &ip_buffer );
-	np = matrix_size/bsp_nprocs();
-	//double x_array[] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.10,0.11,0.12};
-	x = vector+(bsp_pid()*np);
-	y = matrix;
-	y = y + count*matrix_size + (bsp_pid()*np);
-	bsp_sync();
+	double *tmp_pagerank = malloc(matrix_size * sizeof(double));
 
-	//printf("1e element(k=%d) in thread %d van y= %f\n",k,bsp_pid(),*y);
-	double alpha = ip( x, y, ip_buffer, np );
-	alpha+= 0.1*((double) 1/matrix_size); // this is now the vector-matrix product of pi(stationary vector and P (stochastic matrix) + E
-	//printf("alpha thread %d = %f met k=%d\n",bsp_pid(),alpha,count);
-	
-	//printf("alpha van thread %d = %f.(k= %d)\n",bsp_pid(),alpha,k);
+	// Setup the initial PageRank vector (all pages have equal rank)
+	for (int idx = 0; idx < matrix_size; idx++) {
+		tmp_pagerank[idx] = size_div;
+	}
+
+	// Register PageRank vector in BSP
+	bsp_push_reg(tmp_pagerank, matrix_size * sizeof(double));
+
+	for (unsigned int it = 0; it < ITERATIONS; it++) {
+		for (int col = offsets[bsp_pid()], pos = col * matrix_size;
+				col < offsets[bsp_pid() + 1]; col++) {
+			double rank = 0;
+			for (int idx = 0; idx < matrix_size; idx++) {
+				rank += tmp_pagerank[idx] * google_matrix[pos];
+				pos++;
+			}
+			for (unsigned int pid = 0; pid < nprocs; pid++ ) {
+				printf("%d: %d, %f\n", pid, col, rank);
+				bsp_put(pid, &rank, tmp_pagerank, col * sizeof(double), sizeof(double));
+			}
+		}
+		bsp_sync();
+	}
+
+	for (int col = offsets[bsp_pid()]; col < offsets[bsp_pid() + 1]; col++) {
+		pagerank_vector[col] = tmp_pagerank[col];
+	}
+
+	bsp_pop_reg(tmp_pagerank);
+	//free(tmp_pagerank);
+
+	// End the BSP program
 	bsp_end();
-	vector_tmp[count] = alpha;
 }
 
 int main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("Missing matrix size parameter\n");
-		return 0;
+	size_t nprocs = bsp_nprocs();
+
+	// Setup
+	matrix_size = 6;
+	size_div = (double) 1 / matrix_size;
+	google_matrix = malloc(matrix_size * matrix_size * sizeof(double));
+	nonzero_vector = malloc(matrix_size * sizeof(size_t));
+	offsets = malloc((nprocs + 1) * sizeof(size_t));
+	pagerank_vector = malloc(matrix_size * sizeof(double));
+
+	// Calculate row offsets for each thread
+	size_t rows_min = matrix_size / nprocs;
+	size_t rows_rem = matrix_size % nprocs;
+	offsets[0] = 0;
+	for (int idx = 0; idx < nprocs; idx++) {
+		offsets[idx + 1] = offsets[idx] + rows_min + (idx < rows_rem ? 1 : 0);
 	}
 
-	matrix_size = atoi(argv[1]);
-	
-	for(unsigned int l = 0;l < matrix_size;l++)
-		vector[l] = (double) 1/matrix_size;
+	// Temporary test matrix (transposed)
+	double test_matrix[] = {
+		0, 0, 1, 0, 0, 0,
+		1, 0, 1, 0, 0, 0,
+		1, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 1, 1,
+		0, 0, 1, 1, 0, 0,
+		0, 0, 0, 1, 1, 0
+	};
+	size_t test_nonzero[] = {
+		2, 0, 3, 2, 2, 1
+	};
 
+	// Read link matrix and nonzero vector (to be replaced with matrix market)
+	for (int idx = 0; idx < matrix_size * matrix_size; idx++) {
+		google_matrix[idx] = test_matrix[idx];
+	}
+	for (int idx = 0; idx < matrix_size; idx++) {
+		nonzero_vector[idx] = test_nonzero[idx];
+	}
+
+	// Start Google matrix BSP program
 	clock_t start = clock();
-	//start
-	for(unsigned int w = 0;w<50;w++){ // Power method
-		count = 0;
-		for(int k = 0;k < matrix_size;k++){
-			bsp_init( &spmd, argc, argv );
-			spmd();
-			count++;
-		}
-		memcpy(&vector,&vector_tmp,8*matrix_size);
-	}
-	//end
+	bsp_init(&gm_spmd, argc, argv);
+	gm_spmd();
 	clock_t end = clock();
 
-	// Calculate time it took
+	// Write time elapsed to build Google matrix
 	double elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Time taken: %lfs\n", elapsed);
+	printf("Google matrix built in %lfs\n", elapsed);
 
-	for(unsigned int o = 0;o < matrix_size;o++)
-		printf("Stationary vector [%d] = %f\n",o,vector_tmp[o]);
+	// Start PageRank BSP
+	start = clock();
+	bsp_init(&pr_spmd, argc, argv);
+	pr_spmd();
+	end = clock();
+
+	// Write time elapsed to calculate the PageRank vector
+	elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Google PageRank calculated in %lfs\n", elapsed);
+
+	// Write the PageRank vector
+	printf("PageRank vector:\n(%f", pagerank_vector[0]);
+	for (int idx = 1; idx < matrix_size; idx++) {
+		if (idx % 5 == 0) {
+			printf("\n");
+		}
+		printf(", %f", pagerank_vector[idx]);
+	}
+	printf(")\n");
 }
-
-
